61ad6b6377a363d04471274a8971e239c6ee37d4
zephyrZhang <19932726377@163.com>
2024-11-18 17:12:48 +0800

    集合作业3

---
+++ JavaOnline1118/src/main/java/com/personal/study/Demo3.java
@@ -0,0 +1,12 @@
+package com.personal.study;
+
+public class Demo3 {
+    public static void main(String[] args) {
+        String[] strings = new String[5];
+        strings[0] = "happy every day";
+        strings[1] = "good morning";
+        strings[2] = "what is happened";
+        strings[3] = "so";
+        strings[4] = "you are so stupid";
+    }
+}
