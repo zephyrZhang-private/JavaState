eafa2d6c6c11af706edf13a6c7dc655d16394bb5
zephyrZhang <19932726377@163.com>
2024-11-29 11:43:46 +0800

    IO Stream -- BufferedReader | BufferedWriter

--- JavaOnline1127/src/com/personal/study/Demo6.java
+++ JavaOnline1127/src/com/personal/study/Demo6.java
@@ -21,15 +21,41 @@ public class Demo6 {
     public static void main(String[] args) throws IOException {
 //      1.缓冲流不能读写，只能提高效率，需要借助普通流实现读入、写出操作
 
-        BufferedReader bufferedReader = new BufferedReader(new FileReader("a.txt"));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("e.txt"));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("e.txt"));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("f.txt"));
 
 //      2.完成读写 -- 方法与普通流一样
 //        一次读取一个字符
         int num = 0;
-        while ((num=bufferedReader.read()) != -1){
-            bufferedWriter.write(num);
+//        while ((num=bufferedReader.read()) != -1){
+//            bufferedWriter.write(num);
+//        }
+
+//        一次读取多个字符
+//        char[] chars = new char[2];
+//        while ((num=bufferedReader.read(chars)) != -1){
+//            bufferedWriter.write(chars,0,num);
+//        }
+
+//        字符缓冲流特有：一次读取一行；字节缓冲流没有
+        /*
+        readline
+        返回值是当前行的内容，数据读完时，返回null
+        原理：遇到换行符，停止读,不读换行符
+        需要补充换行符
+         */
+        String data;
+        while ((data = bufferedReader.readLine()) != null){
+            bufferedWriter.write(data);
+          /*
+          平台不一样,换行符的表示不一样
+            1.\r\n
+            2.\n
+          解决：newline,跨平台的换行方式
+           */
+            bufferedWriter.newLine();
         }
+
 
 //      3.关闭流，此处未实现自动关闭流功能
         bufferedReader.close();
