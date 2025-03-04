6521ea7a2e9f215ad11c3f6415f26c806e4ffce2
zephyrZhang <19932726377@163.com>
2025-03-04 14:13:48 +0800

    JDBC execute Query & Update

--- JavaSECode/src/personal/jdbc/Demo1.java
+++ JavaSECode/src/personal/jdbc/Demo1.java
@@ -1,26 +1,74 @@
 package personal.jdbc;
 
 import java.sql.*;
+import java.util.Properties;
+import java.util.StringTokenizer;
+
+/*
+JDBC的基本使用
+
+步骤：
+1.导入资源--添加jar包到Libraries
+2.注册驱动
+3.建立连接
+4.获取statement对象并创建sql
+5.获取ResultSet对象并卸货 -- 查询对应executeQuery
+6.关闭资源
+ */
 
 public class Demo1 {
     public static void main(String[] args) throws SQLException {
+//      2.注册驱动，获取驱动对象
+//      mysql8.0--com.mysql.jdbc.Driver
+//      mysql5.7--com.mysql.cj.jdbc.Driver
         DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-//        Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.61.11:21001/zhdb?useSSL=false","root","root");
-//        System.out.println("successfully "+connection.getMetaData().getDatabaseProductVersion());
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_1","root","root");
-        System.out.println("successfully " +connection.getMetaData().getDatabaseProductVersion());
+
+//      3.建立连接  -- 父协议:子协议 ： jdbc:mysql://域名(ip):端口/dbname？useSSL=false
+//      网络通信三要素 -- 协议 域名 端口
+
+//      三个参数
+        Connection connection = DriverManager.getConnection("jdbc:mysql://10.1.111.221:3306/zhdb","root","root");
+        System.out.println("successfully "+connection.getMetaData().getDatabaseProductVersion());
+
+//      两个参数 -- properties对象存储键值对
+//        Properties properties = new Properties();
+//        properties.setProperty("user", "root");
+//        properties.setProperty("password", "root");
+//        Connection connection1 = DriverManager.getConnection("jdbc:msyql://47.239.15.195:3306/wordpress?useSSL=false",properties);
+
+//      一个参数 -- 使用?判断用户名密码并&连接
+//        Connection connection1 = DriverManager.getConnection("jdbc:mysql://47.239.15.195:3306/wordpress?user=root&password=root");
+
+//      4.获取statement对象并传入sql
         Statement statement = connection.createStatement();
-        String sql = "select * from t_china";
+        String sql = "select * from information_schema.tables where table_schema = 'zhdb'";
+
+
+//      5.获取ResultSet对象并卸货
         ResultSet set = statement.executeQuery(sql);
         while (set.next()){
-            Object o1 = set.getObject("name");
-            Object o2 = set.getObject("value");
-            Object o3 = set.getObject("parent");
-            System.out.println(o1+"-"+o2+"-"+o3);
+//          根据字段名字获取字段值
+            Object o1 = set.getObject("table_name");
+            System.out.println(o1);
         }
-        System.out.println();
+
+        String sql1 = "select * from t_china";
+        ResultSet set1 = statement.executeQuery(sql1);
+        while (set1.next()){
+//          根据字段的下标获取字段值，下标从1开始，而且这里对应的表是虚拟表
+//            Object o2 = set1.getObject(1);
+//            System.out.println(o2);
+//          根据字段名获取数据
+            String name = set1.getString("name");
+            String addressno = set1.getString("value");
+            String address = set1.getString("parent");
+            System.out.println(name + " " + addressno + " " + address);
+        }
+
+//      6.关闭资源
         connection.close();
         statement.close();
         set.close();
+        set1.close();
     }
 }
---
+++ JavaSECode/src/personal/jdbc/Demo2.java
@@ -0,0 +1,55 @@
+package personal.jdbc;
+
+import java.sql.*;
+
+/*
+JDBC初级优化
+
+步骤：
+1.导入资源
+2.注册驱动
+3.建立连接
+4.statement创建sql
+5.查询对应executeQuery  增删改对应executeUpdate
+6.关闭资源
+ */
+
+public class Demo2 {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+/*    2.注册驱动，获取驱动对象
+       - mysql8.0--com.mysql.jdbc.Driver
+       - mysql5.7--com.mysql.cj.jdbc.Driver
+      简化,通过类静态方法引用,减少对象重复new的过程
+       - 减少资源的浪费
+       - 提升工作效率
+*/
+//        DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+/*      3.建立连接
+         - 父协议:子协议
+           jdbc:mysql://域名(ip):端口/dbname？useSSL=false
+         - 网络通信三要素
+           协议 域名 端口
+*/
+        Connection connection = DriverManager.getConnection("jdbc:mysql://10.1.111.221:3306/zhdb","root","root");
+        System.out.println("successfully "+connection.getMetaData().getDatabaseProductVersion());
+
+//      4.获取statement对象并传入sql
+        Statement statement = connection.createStatement();
+        String sql = "insert into t_china(name,value,parent) values('张伟','28','张立动')";
+
+//      5.获取执行结果
+
+        int i = statement.executeUpdate(sql);
+        if (i > 0) {
+            System.out.println("insert success");
+        }else {
+            System.out.println("insert fail");
+        }
+
+//      6.关闭资源
+        statement.close();
+        connection.close();
+    }
+}
