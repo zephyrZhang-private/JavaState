102e8d360732e520298966b12be116e824fdb87f
zephyrZhang <19932726377@163.com>
2025-03-04 16:37:49 +0800

    Class Entry

---
+++ JavaSECode/src/personal/jdbc/Demo5.java
@@ -0,0 +1,56 @@
+package personal.jdbc;
+
+import util.entry.TChina;
+import util.jdbcutils.JDBCUtils;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+JDBC的模型封装
+ */
+
+public class Demo5 {
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet set = null;
+        try {
+            connection = JDBCUtils.getConnection();
+            System.out.println("successfully " + connection.getMetaData().getDatabaseProductVersion());
+
+//      4.获取statement对象并传入sql
+            statement = connection.createStatement();
+            String sql = "select * from t_china";
+
+//      5.获取ResultSet对象并卸货
+            List<TChina> list= new ArrayList<>();
+            set = statement.executeQuery(sql);
+            while (set.next()) {
+//              根据字段名获取数据
+                String name = set.getString("name");
+                String addressno = set.getString("value");
+                String address = set.getString("parent");
+                TChina tChina = new TChina(name, address, addressno);
+                list.add(tChina);
+/*
+       模型封装:封装到一个类,数据库表中的每一行封装成一个实体
+       封装类的特点
+        - 类名与表名一致
+        - 类中的属性与表的字段名字一致
+       这种对应表中每行数据的类称为模型类
+ */
+            }
+            System.out.println(list);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            JDBCUtils.closeAll(connection, statement, set);
+        }
+    }
+}
---
+++ JavaSECode/src/util/entry/TChina.java
@@ -0,0 +1,62 @@
+package util.entry;
+
+import java.io.Serializable;
+
+/*
+模型类的几个必须要素
+ - 私有的属性
+ - 对应的get、set方法
+ - 空参、有参构造方法
+ - toString方法
+ - 序列化
+ */
+
+public class TChina implements Serializable {
+
+    private final long serialVersionUID = 4124564512L;
+
+    private String name;
+    private String value;
+    private String parent;
+
+
+    @Override
+    public String toString() {
+        return "TChina{" +
+                "name='" + name + '\'' +
+                ", value='" + value + '\'' +
+                ", parent='" + parent + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getParent() {
+        return parent;
+    }
+
+    public void setParent(String parent) {
+        this.parent = parent;
+    }
+
+    public TChina() {}
+    public TChina(String name, String value, String parent) {
+        this.name = name;
+        this.value = value;
+        this.parent = parent;
+    }
+}
