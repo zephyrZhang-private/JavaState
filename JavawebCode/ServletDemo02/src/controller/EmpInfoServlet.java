6e85df6e258529c7504f9220e33a3f2a54d029d8
zhangweiTemp <19932726377@163.com>
2025-08-04 18:10:43 +0800

    ServletDemo02 Update

--- JavawebCode/ServletDemo02/src/controller/EmpInfoServlet.java
+++ JavawebCode/ServletDemo02/src/controller/EmpInfoServlet.java
@@ -18,7 +18,8 @@ public class EmpInfoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        System.out.println("empall---EmpServlet");
+        resp.setContentType("text/html;charset=UTF-8");
+        System.out.println("empInfo---EmpServlet");
         //展示所有雇员的信息
         EmpInfoService empService = new EmpInfoServiceImpl();
         List<EmpInfo> list = empService.getEmpInfo();
