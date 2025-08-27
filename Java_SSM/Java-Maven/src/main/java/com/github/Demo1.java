cfa9d840d121297f5f0e3a114c31882f88ecd3f3
zephyrZhang <19932726377@163.com>
2025-08-27 18:28:06 +0800

    Maven Basic

---
+++ Java_SSM/Java-Maven/src/main/java/com/github/Demo1.java
@@ -0,0 +1,7 @@
+package com.github;
+
+public class Demo1 {
+    public void hello() {
+        System.out.println("demo1");
+    }
+}
---
+++ Java_SSM/Java-Maven/src/test/java/com/github/TestDemo.java
@@ -0,0 +1,11 @@
+package com.github;
+
+import org.junit.Test;
+
+public class TestDemo {
+    @Test
+    public void test() {
+        Demo1 demo1 = new Demo1();
+        demo1.hello();
+    }
+}
